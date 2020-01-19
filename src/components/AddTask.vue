c649a0d95ea0841aab9390e6bb1a0c14c7b07c1e
Vlad Bogdan <vlad.bogdan90@gmail.com>
2020-01-19 17:45:41 +0100

    handle post request

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="nes-field">
     <label for="name_field">Add new task</label>
-    <input type="text" id="name_field" class="nes-input">
-    <button type="button" class="nes-btn is-primary">Add</button>
+    <input v-model.lazy="data.name" type="text" id="name_field" class="nes-input" />
+    <button @click="saveTask" type="button" class="nes-btn is-primary">Add</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {
+        name: '',
+        status: ''
+      },
+      errors: [],
+      response: []
+    };
+  },
+  methods: {
+    saveTask() {
+      this.$http
+        .post('http://localhost:8000/todo/create', this.data)
+        // eslint-disable-next-line no-console
+        .then(response => this.response.push(response))
+        .catch(e => {
+          this.errors.push(e)
+        })
+    }
+  }
+};
+</script>
