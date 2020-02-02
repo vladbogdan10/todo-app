8aa240d02df4ba1047aaeab992f5623f308e4762
Vlad Bogdan <vlad.bogdan90@gmail.com>
2020-02-02 14:56:36 +0100

    import deleteTask component

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,16 +1,23 @@
 <template>
   <ul>
-    <li v-for="task in data" :key="task.id" class="nes-container is-rounded with-title">
+    <li v-for="task in data" :key="task.id" :id="task.id" class="nes-container is-rounded with-title">
       <span v-if="task.done" class="title nes-text is-success">Done</span>
       <span v-if="task.inProgress" class="title nes-text is-primary">Progress</span>
       <p>{{ task.name }}</p>
+      <DeleteTask @deleteTask="$emit('deleteTask', task.id)" />
     </li>
   </ul>
 </template>
 
 <script>
+import DeleteTask from "../components/DeleteTask"
+
 export default {
   name: "Task",
+  components: {
+    DeleteTask
+  },
+
   props: {
     data: Array
   }
