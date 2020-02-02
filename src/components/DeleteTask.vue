f21d45c91e63661f81e8f54de9bb947b26ba7c8b
Vlad Bogdan <vlad.bogdan90@gmail.com>
2020-02-02 14:54:05 +0100

    add deleteTask component

---
+++ src/components/DeleteTask.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="deleteTask" type="button" class="nes-btn is-error"><b>&#10005;</b></button>
+</template>
+
+<script>
+export default {
+  name: "DeleteTask",
+
+  data() {
+    return {
+      response: [],
+      errors: [],
+      taskId: ''
+    }
+  },
+
+  methods: {
+    deleteTask(e) {
+      this.taskId = e.target.parentNode.attributes.id.value;
+
+      // this.$http
+      //   .delete(`http://localhost:8000/todo/delete/${taskId}`)
+      //   .then()
+      //   .catch(e => this.errors.push(e))
+      this.deleteFromList()
+    },
+
+    deleteFromList() {
+      this.$emit('deleteTask', this.taskId)
+    }
+  }
+};
+</script>
