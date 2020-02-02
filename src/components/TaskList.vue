4bfb717548bca12ba066517c0291beeb1983f839
Vlad Bogdan <vlad.bogdan90@gmail.com>
2020-02-02 11:45:52 +0100

    create TaskList component

---
+++ src/components/TaskList.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="task-list nes-container is-error with-title">
+    <h3 class="title">Tasks</h3>
+    <Task v-bind="$props" />
+  </section>
+</template>
+
+<script>
+import Task from '../components/Task.vue'
+
+export default {
+  name: "TaskList",
+  components: {
+    Task
+  },
+
+  props: {
+    data: Array
+  }
+};
+</script>
