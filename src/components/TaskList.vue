369abcaea883596b9c09120829fa835886db5c9b
Vlad Bogdan <vlad.bogdan90@gmail.com>
2020-02-02 14:57:19 +0100

    data to parent

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="task-list nes-container is-error with-title">
     <h3 class="title">Tasks</h3>
-    <Task v-bind="$props" />
+    <Task v-bind="$props" @deleteTask="deleteTask" />
   </section>
 </template>
 
@@ -16,6 +16,12 @@ export default {
 
   props: {
     data: Array
+  },
+
+  methods: {
+    deleteTask(taskId) {
+      this.$emit('deleteTask', taskId)
+    }
   }
 };
 </script>
