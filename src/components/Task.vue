d0616ad1b24221db7024161e76bca67d5aa44c75
Vlad Bogdan <vlad.bogdan90@gmail.com>
2020-02-09 16:24:57 +0100

    component refactoring

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,54 +1,36 @@
 <template>
-  <ul>
-    <li v-for="(task, index) in data" :key="task.id" class="nes-container is-rounded with-title">
-      <span v-if="task.done" class="title nes-text is-success">Done</span>
-      <span v-if="task.inProgress" class="title nes-text is-primary">Progress</span>
-      <p>{{ task.name }}</p>
-      <DeleteTask :index="index" :task="task" @removeFromList="removeFromList" />
-    </li>
-  </ul>
+  <div class="nes-container is-rounded with-title">
+    <span v-if="task.done" class="title nes-text is-success">Done</span>
+    <span v-if="task.inProgress" class="title nes-text is-primary">Progress</span>
+    <p :class="{done: task.done}">{{ task.name }}</p>
+  </div>
 </template>
 
 <script>
-import DeleteTask from "../components/DeleteTask"
-
 export default {
   name: "Task",
-  components: {
-    DeleteTask
-  },
 
   props: {
-    data: Array
-  },
-
-  methods: {
-    removeFromList(index){
-      this.$delete(this.data, index)
-    }
+    task: Object,
+    index: Number
   }
 }
 </script>
 
 <!-- SCOPED STYLES -->
 <style lang="scss" scoped>
-  ul {
-    padding-left: 0;
-
-    li {
-      list-style: none;
-
-      &:not(:first-child) {
-        margin-top: 1em;
-      }
-    }
-  }
-
   .nes-container {
+    flex-basis: 100%;
+
     .with-title {
       .title {
         font-size: 12px;
       }
     }
+  }
+
+  .done {
+    text-decoration: line-through;
+    color:darkgrey
   }
 </style>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,21 +1,54 @@
 <template>
-  <section class="task-list nes-container is-error with-title">
-    <h3 class="title">Tasks</h3>
-    <Task v-bind="$props" />
+  <section class="task-list nes-container with-title">
+    <h3 class="title">TASK LIST</h3>
+    <ul>
+      <li v-for="(task, index) in taskList" :key="task.id">
+        <Task :task="task" :index="index" />
+        <DeleteTask :index="index" :task="task" @removeFromList="removeFromList" />
+      </li>
+    </ul>
   </section>
 </template>
 
 <script>
 import Task from '../components/Task.vue'
+import DeleteTask from '../components/DeleteTask.vue'
 
 export default {
   name: "TaskList",
   components: {
-    Task
+    Task,
+    DeleteTask
   },
 
   props: {
-    data: Array
+    taskList: Array
+  },
+
+  methods: {
+    removeFromList(index){
+      this.$delete(this.taskList, index)
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .task-list {
+    margin-top: 3em;
+  }
+
+  ul {
+    // padding-left: 0;
+    padding: 2em 0 0 0;
+
+    li {
+      display: flex;
+      list-style: none;
+
+      &:not(:first-child) {
+        margin-top: 1.2em;
+      }
+    }
+  }
+</style>
